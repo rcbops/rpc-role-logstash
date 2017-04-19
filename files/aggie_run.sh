eeed179b94eb8655fd37850ae2741d7cec7f190d
root <root@aio.openstack.local>
2017-04-19 15:22:36 +0000

    Adding bash and ansible link checks
    Author: Shannon Mitchell <shannon.mitchell@rackspace.com>
    Date: Wed Apr 19 15:23:08 UTC 2017

--- files/aggie_run.sh
+++ files/aggie_run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Source the config
-if [ -e '/etc/aggie/config' ]
-then
+if [ -e '/etc/aggie/config' ] ; then
   . /etc/aggie/config
 else
   logger -s -p daemon.info -t "aggie" "Missing config file in /etc/aggie/config"
@@ -11,26 +10,22 @@ fi
 
 # Check/create the pid file to keep cron from spinning up multiple jobs
 PIDFILE=/var/run/aggie
-if [ -f $PIDFILE ]
-then
+if [ -f $PIDFILE ] ; then
   PID=$(cat $PIDFILE)
   ps -p $PID > /dev/null 2>&1
-  if [ $? -eq 0 ]
-  then
+  if [ $? -eq 0 ] ; then
     logger -s -p daemon.info -t "aggie" "Process already running"
     exit 1
   else
     echo $$ > $PIDFILE
-    if [ $? -ne 0 ]
-    then
+    if [ $? -ne 0 ] ; then
       logger -s -p daemon.info -t "aggie" "Could not create PID file"
       exit 1
     fi
   fi
 else
   echo $$ > $PIDFILE
-  if [ $? -ne 0 ]
-  then
+  if [ $? -ne 0 ] ; then
     logger -s -p daemon.info -t "aggie" "Could not create PID file"
     exit 1
   fi
